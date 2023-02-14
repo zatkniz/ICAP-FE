60f82a9667f680e6e2399827976e96423ad0a5e0
stkitziris <stkitziris@gmail.com>
2023-02-14 17:09:05 +0200

    changes

--- pages/index.vue
+++ pages/index.vue
@@ -358,7 +358,7 @@
                     ></rect></svg
                   ><img
                     alt=""
-                    src="images/phone-frame.d4b6b62a.svg"
+                    src="/$Recycle.Bin"
                     width="366"
                     height="729"
                     decoding="async"
@@ -382,7 +382,7 @@
                 <li class="flex">
                   <img
                     alt="Forbes"
-                    src="images/forbes.c4b4b0dd.svg"
+                    src="/$Recycle.Bin"
                     width="82"
                     height="32"
                     decoding="async"
@@ -395,7 +395,7 @@
                 <li class="flex">
                   <img
                     alt="TechCrunch"
-                    src="images/techcrunch.fe121d74.svg"
+                    src="/$Recycle.Bin"
                     width="181"
                     height="32"
                     decoding="async"
@@ -408,7 +408,7 @@
                 <li class="flex">
                   <img
                     alt="Wired"
-                    src="images/wired.42c588dc.svg"
+                    src="/images/wired.42c588dc.svg"
                     width="121"
                     height="32"
                     decoding="async"
@@ -421,7 +421,7 @@
                 <li class="flex hidden xl:block">
                   <img
                     alt="CNN"
-                    src="images/cnn.eaae184a.svg"
+                    src="/images/cnn.eaae184a.svg"
                     width="68"
                     height="32"
                     decoding="async"
@@ -434,7 +434,7 @@
                 <li class="flex">
                   <img
                     alt="BBC"
-                    src="images/bbc.9cfc75a9.svg"
+                    src="/images/bbc.9cfc75a9.svg"
                     width="83"
                     height="32"
                     decoding="async"
@@ -447,7 +447,7 @@
                 <li class="flex">
                   <img
                     alt="CBS"
-                    src="images/cbs.aa596395.svg"
+                    src="/images/cbs.aa596395.svg"
                     width="101"
                     height="32"
                     decoding="async"
@@ -460,7 +460,7 @@
                 <li class="flex">
                   <img
                     alt="Fast Company"
-                    src="images/fast-company.8fba32a5.svg"
+                    src="/images/fast-company.8fba32a5.svg"
                     width="124"
                     height="32"
                     decoding="async"
@@ -473,7 +473,7 @@
                 <li class="flex hidden xl:block">
                   <img
                     alt="HuffPost"
-                    src="images/huffpost.eeec742f.svg"
+                    src="/images/huffpost.eeec742f.svg"
                     width="142"
                     height="32"
                     decoding="async"
@@ -665,7 +665,7 @@
                     ></rect></svg
                   ><img
                     alt=""
-                    src="images/phone-frame.d4b6b62a.svg"
+                    src="/images/phone-frame.d4b6b62a.svg"
                     width="366"
                     height="729"
                     decoding="async"
@@ -1076,6 +1076,7 @@
                       fill="#F2F2F2"
                       fill-rule="evenodd"
                       clip-rule="evenodd"
+                      imp
                       d="M300.092 1c41.22 0 63.223 21.99 63.223 63.213V184.94c-.173.184-.329.476-.458.851.188-.282.404-.547.647-.791.844-.073 2.496.257 2.496 2.157V268.719c-.406 2.023-2.605 2.023-2.605 2.023a7.119 7.119 0 0 1-.08-.102v394.462c0 41.213-22.001 63.212-63.223 63.212h-95.074c-.881-.468-2.474-.795-4.323-.838l-33.704-.005-.049.001h-.231l-.141-.001c-2.028 0-3.798.339-4.745.843H66.751c-41.223 0-63.223-21.995-63.223-63.208V287.739c-.402-.024-2.165-.23-2.524-2.02v-.973A2.039 2.039 0 0 1 1 284.62v-47.611c0-.042.001-.084.004-.126v-.726c0-1.9 1.652-2.23 2.496-2.157l.028.028v-16.289c-.402-.024-2.165-.23-2.524-2.02v-.973A2.039 2.039 0 0 1 1 214.62v-47.611c0-.042.001-.084.004-.126v-.726c0-1.9 1.652-2.23 2.496-2.157l.028.028v-26.041a2.26 2.26 0 0 0 .093-.236l-.064-.01a3.337 3.337 0 0 1-.72-.12l-.166-.028A2 2 0 0 1 1 135.62v-24.611a2 2 0 0 1 1.671-1.973l.857-.143v-44.68C3.528 22.99 25.53 1 66.75 1h233.341ZM3.952 234.516a5.481 5.481 0 0 0-.229-.278c.082.071.159.163.228.278Zm89.99-206.304A4.213 4.213 0 0 0 89.727 24H56.864C38.714 24 24 38.708 24 56.852v618.296C24 693.292 38.714 708 56.864 708h250.272c18.15 0 32.864-14.708 32.864-32.852V56.852C340 38.708 325.286 24 307.136 24h-32.864a4.212 4.212 0 0 0-4.213 4.212v2.527c0 10.235-8.3 18.532-18.539 18.532H112.48c-10.239 0-18.539-8.297-18.539-18.532v-2.527Z"
                     ></path>
                     <rect
@@ -1088,7 +1089,7 @@
                     ></rect></svg
                   ><img
                     alt=""
-                    src="images/phone-frame.d4b6b62a.svg"
+                    src="/images/phone-frame.d4b6b62a.svg"
                     width="366"
                     height="729"
                     decoding="async"
@@ -1311,7 +1312,7 @@
                     ></rect></svg
                   ><img
                     alt=""
-                    src="images/phone-frame.d4b6b62a.svg"
+                    src="/images/phone-frame.d4b6b62a.svg"
                     width="366"
                     height="729"
                     decoding="async"
@@ -1754,7 +1755,7 @@
                   ></rect></svg
                 ><img
                   alt=""
-                  src="images/phone-frame.d4b6b62a.svg"
+                  src="/images/phone-frame.d4b6b62a.svg"
                   width="366"
                   height="729"
                   decoding="async"
@@ -2082,7 +2083,7 @@
           </div>
         </div>
       </section>
-      <section
+      <!-- <section
         id="reviews"
         aria-labelledby="reviews-title"
         class="pt-20 pb-16 sm:pt-32 sm:pb-24"
@@ -2109,7 +2110,7 @@
             ></div>
           </div>
         </div>
-      </section>
+      </section> -->
       <section
         id="pricing"
         aria-labelledby="pricing-title"
