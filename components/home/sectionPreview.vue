7ae5f9cbf857835299a9035a7f7cfe31a4b1c389
stkitziris <stkitziris@gmail.com>
2023-03-07 13:42:18 +0200

    small changes

--- components/home/sectionPreview.vue
+++ components/home/sectionPreview.vue
@@ -34,7 +34,7 @@
         >
           <nuxt-link
             to="contact-us"
-            class="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            class="rounded-full bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >Ας ξεκινήσουμε</nuxt-link
           >
           <nuxt-link
